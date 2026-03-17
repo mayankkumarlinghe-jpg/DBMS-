19b0ee94ef0e3d7061c93a89ca687ece8d5dd3bd
mayankkumarlinghe-jpg <mayankkumarlinghe@gmail.com>
2026-03-17 18:00:00 +0530

    Docs: Add explanatory comments to Problem 176 query

--- 176Leetcode.sql
+++ 176Leetcode.sql
@@ -1,3 +1,7 @@
+-- Problem 176: Second Highest Salary
+-- This query finds the second highest salary from the Employee table.
+-- It uses a subquery to first find the absolute highest salary, 
+-- and then selects the maximum salary that is strictly less than that highest value.
 SELECT MAX(salary) AS SecondHighestSalary
 FROM Employee
 WHERE salary < (SELECT MAX(salary) FROM Employee);
